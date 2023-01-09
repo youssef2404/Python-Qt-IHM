50637c88287a8719e8ef55724980b30f79bc7a7d
Tabarki youssef <73352847+youssef2404@users.noreply.github.com>
2023-01-09 11:01:29 +0000

    Delete Edge_Box/generate_application directory

--- Edge_Box/generate_application/generate_EdgeBox_application.bat
+++
@@ -1,7 +0,0 @@
-python -m PyInstaller --onefile --name EdgeBoxApplication --distpath ..\Edge_Box_exe --windowed --icon=..\icons\addixo_X_icon.ico ..\main_user_interface.py
-
-xcopy /e /k /h /i /y ..\icons ..\Edge_Box_exe\icons
-xcopy /e /k /h /i /y build ..\Edge_Box_exe
-xcopy /e /k /h /i /y EdgeBoxApplication.spec ..\Edge_Box_exe
-del /f EdgeBoxApplication.spec
-rmdir /s /q build
--- Edge_Box/generate_application/install_required_packages.bat
+++
@@ -1,4 +0,0 @@
-python -m pip install --upgrade pip
-pip install -r required_packages.txt
-
-timeout 10
